6237da932de805cc03c5a5d3dd93f07fd6c12aaf
Kveykva04 <taradwright04@gmail.com>
2022-11-14 21:33:59 -0500

    updated human object and getting data from the form

--- app.js
+++ app.js
@@ -23,21 +23,41 @@
     };
 
     // Create Human Object
-
+    const human = {
+        name: "",
+        heightFeet: 0,
+        heightInches: 0,
+        weight: 0,
+        diet: ""
+    }
     // Use IIFE to get human data from form
-
+    const button = document.getElementById("btn");
+    button.addEventListener('click', (function() {
+        human.name = document.getElementById("name").value;
+        human.heightFeet = document.getElementById("feet").value;
+        human.heightInches = document.getElementById("inches").value;
+        human.weight = document.getElementById("weight").value;
+        human.diet = document.getElementById("diet").value;
+    })());
+    console.log(human);
 
     // Create Dino Compare Method 1
     // NOTE: Weight in JSON file is in lbs, height in inches. 
-
+    function compare1(){
+            
+    }
     
     // Create Dino Compare Method 2
     // NOTE: Weight in JSON file is in lbs, height in inches.
-
+    function compare2(){
+        
+    }
     
     // Create Dino Compare Method 3
     // NOTE: Weight in JSON file is in lbs, height in inches.
-
+    function compare3(){
+        
+    }
 
     // Generate Tiles for each Dino in Array
   
